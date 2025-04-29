f42e567e30af2d7322b5c72bd1f899e4f1b94424
Davian <davianr@hotmail.co.uk>
2025-04-29 18:47:16 +0100

    Refactor project structure; enhance styles and components for improved UI in Israel Kitchen app

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Israel Kitchen";
+export const APP_DESCRIPTION= process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern and easy to use Israel Kitchen app";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000";
+export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || "1.0.0";
+export const APP_AUTHOR = process.env.NEXT_PUBLIC_APP_AUTHOR || "Israel Kitchen Team";
+export const APP_AUTHOR_URL = process.env.NEXT_PUBLIC_APP_AUTHOR_URL || "https://israel-kitchen.vercel.app";
+export const APP_AUTHOR_EMAIL = process.env.NEXT_PUBLIC_APP_AUTHOR_EMAIL || "israel-kitchen@gmail.com";
